3b50e4e7366ee97b9793780b5d3a18345156a35a
Drenmax7 <92438713+Drenmax7@users.noreply.github.com>
2025-10-21 21:18:02 +0200

    add goat support to differenciate normal from screaming ones
    fix panda color text that was not working
    add color to horse text
    
    add cooldown to the message display to prevent lag, caused by player holding for too long the right click button

--- src/main/java/net/x5h4d0wx/animalinfo/entity/AnimalInteractionListener.java
+++ src/main/java/net/x5h4d0wx/animalinfo/entity/AnimalInteractionListener.java
@@ -4,19 +4,22 @@ package net.x5h4d0wx.animalinfo.entity;
 import net.fabricmc.fabric.api.event.player.UseEntityCallback;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.passive.GoatEntity;
 import net.minecraft.entity.passive.PandaEntity;
 import net.minecraft.entity.passive.AbstractHorseEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import net.minecraft.text.Texts;
 import net.minecraft.util.ActionResult;
-import net.minecraft.util.Formatting;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -28,6 +31,8 @@ public class AnimalInteractionListener implements UseEntityCallback {
     public static AtomicBoolean exec_scheduled = new AtomicBoolean(false);
     public static AtomicBoolean exec_ended = new AtomicBoolean(true);
     private static ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(2);
+
+    final private static Map<Integer, Long> cooldownEntity = new HashMap<>();
     
     public AnimalInteractionListener(){
         executor.setMaximumPoolSize(2);
@@ -38,36 +43,58 @@ public class AnimalInteractionListener implements UseEntityCallback {
         // CommandDispatcher<FabricClientCommandSource> test = ClientCommandManager.getActiveDispatcher();
         // player.sendMessage(Text.of(String.valueOf(test==null)), true);
         exec_scheduled.set(false);
-        if (player.hasVehicle() || player.isSneaking()) {
-            switch (entity) {
-                case PandaEntity panda:
-                    // From the original Panda Info 
-                    outText = Texts.join(List.of(
-                            Text.translatable("gui.panda-info.main_gene",
-                                    Text.translatable("gui.panda-info.gene." + panda.getMainGene()))
-                                    .formatted(Formatting.GOLD),
-                            Text.translatable("gui.panda-info.hidden_gene",
-                                    Text.translatable("gui.panda-info.gene." + panda.getHiddenGene()))
-                                    .formatted(Formatting.AQUA),
-                            Text.translatable("gui.panda-info.product_gene",
-                                    Text.translatable("gui.panda-info.gene." + panda.getProductGene()))
-                                    .formatted(Formatting.GREEN)),
-                            Text.of(" | ")); 
-                    break;
-                case AbstractHorseEntity horsish:
-                    final double blockSpeed = horsish.getAttributeValue(EntityAttributes.MOVEMENT_SPEED)* HORSE_SPEED_CONVERSION;
-                    final double blockJump = jumpStrToBlocks(horsish.getAttributeValue(EntityAttributes.JUMP_STRENGTH));
-                    final double health = horsish.getAttributeValue(EntityAttributes.MAX_HEALTH);
-                    outText = Text.of(String.format("Speed: %1$.2f | Jump %2$.2f | Hearts %3$.2f ",blockSpeed, blockJump, health/2));
 
-                    break;
-                default:
-                    break;
-            }
-            if (outText != null) {
-                sendLingeringMessage(player, entity, outText);
-            }
+        if (!player.hasVehicle() && !player.isSneaking()){
+            return ActionResult.PASS;
         }
+
+        int id = entity.getId();
+        if (cooldownEntity.containsKey(id) && System.currentTimeMillis() < cooldownEntity.get(id)){
+            return ActionResult.PASS;
+        }
+        cooldownEntity.put(id, System.currentTimeMillis() + 200);
+
+        switch (entity) {
+            case PandaEntity panda:
+                // From the original Panda Info
+                MutableText mainGene = Text.translatable("gui.panda-info.gene." + panda.getMainGene());
+                MutableText hiddenGene = Text.translatable("gui.panda-info.gene." + panda.getHiddenGene());
+                MutableText productGene = Text.translatable("gui.panda-info.gene." + panda.getProductGene());
+
+                mainGene = Text.translatable("gui.panda-info.main_gene", "§6", mainGene);
+                hiddenGene = Text.translatable("gui.panda-info.hidden_gene", "§b", hiddenGene);
+                productGene = Text.translatable("gui.panda-info.product_gene", "§a", productGene);
+
+                outText = Texts.join(List.of(mainGene, hiddenGene, productGene), Text.of(" §r| "));
+
+                break;
+
+            case AbstractHorseEntity horsish:
+                final double blockSpeed = horsish.getAttributeValue(EntityAttributes.MOVEMENT_SPEED)* HORSE_SPEED_CONVERSION;
+                final double blockJump = jumpStrToBlocks(horsish.getAttributeValue(EntityAttributes.JUMP_STRENGTH));
+                final double health = horsish.getAttributeValue(EntityAttributes.MAX_HEALTH);
+                outText = Text.of(String.format("§6Speed: %1$.2f §r| §bJump %2$.2f §r| §aHearts %3$.2f ",blockSpeed, blockJump, health/2));
+
+                break;
+
+            case GoatEntity goat:
+                if (goat.isScreaming()){
+                    outText = Text.of("§bScreaming Goat");
+                }
+                else {
+                    outText = Text.of("Normal Goat");
+                }
+
+                break;
+
+            default:
+                break;
+        }
+
+        if (outText != null) {
+            sendLingeringMessage(player, entity, outText);
+        }
+
         return ActionResult.PASS;
     }
 
